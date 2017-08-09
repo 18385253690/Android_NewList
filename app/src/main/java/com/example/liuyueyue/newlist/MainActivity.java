68d882b9d6cd5d4d4b64c5ae9f384f73dc38532f
liuyueyue <838621908@qq.com>
2017-08-09 09:54:30 +0800

    Android_AsyncTask异步加载，获取json数据并解析，BaseAdapter缓存机制，图片异步加载，LruCache缓存，滚动优化。

---
+++ app/src/main/java/com/example/liuyueyue/newlist/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.liuyueyue.newlist;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import static android.os.Build.VERSION_CODES.N;
+
+public class MainActivity extends AppCompatActivity {
+private ListView mListView;
+    private static String URL =
+            "http://www.imooc.com/api/teacher?type=4&num=30";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.lv_main);
+        new NewsAsyncTask().execute(URL);
+    }
+    //将url对应的json格式数据转化为我们所封装的NewsBean
+    private List<NewsBean> getJsonData(String url){
+        List<NewsBean> newsBeanList = new ArrayList<>();
+        try {
+            String JsonString = readStream(new URL(url).openStream());
+            JSONObject jsonObject;
+            NewsBean newsBean;
+            try {
+                jsonObject = new JSONObject(JsonString);
+                JSONArray jsonArray = jsonObject.getJSONArray("data");
+                for(int i =0;i<jsonArray.length();i++){
+                    jsonObject = jsonArray.getJSONObject(i);
+                    newsBean = new NewsBean();
+                    newsBean.newsIconUrl =jsonObject.getString("picSmall");
+                    newsBean.newsTitle = jsonObject.getString("name");
+                    newsBean.newsContent = jsonObject.getString("description");
+                    newsBeanList.add(newsBean);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return newsBeanList;
+
+    }
+    //通过is解析网页返回的数据
+    private String readStream(InputStream is){
+        InputStreamReader isr;
+        String result = "";
+        try {
+            String line = "";
+            isr = new InputStreamReader(is,"utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            while((line=br.readLine())!=null){
+                result += line;
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    //实现了网络的异步访问
+    class NewsAsyncTask extends AsyncTask<String,Void,List<NewsBean>>{
+
+        @Override
+        protected List<NewsBean> doInBackground(String... params) {
+            return getJsonData(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(List<NewsBean> newsBeen) {
+            super.onPostExecute(newsBeen);
+            NewsAdapter adapter = new NewsAdapter(MainActivity.this, newsBeen,mListView);
+            mListView.setAdapter(adapter);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/liuyueyue/newlist/NewsBean.java
@@ -0,0 +1,11 @@
+package com.example.liuyueyue.newlist;
+
+/**
+ * Created by liuyueyue on 2017/8/6.
+ */
+
+public class NewsBean {
+    public String newsIconUrl;
+    public String newsTitle;
+    public String newsContent;
+}
